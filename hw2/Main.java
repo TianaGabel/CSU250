b8132490dcdd8da398c02cebf74d731427fedb5a
Tatiana Gabel <Tianamg78@gmail.com>
2023-09-28 15:49:12 -0600

    added first task code

--- hw2/Main.java
+++ hw2/Main.java
@@ -1,16 +1,24 @@
+import java.util.List;
+
 public class Main {
     
     public static void main(String[] args){
         int size = Integer.parseInt(args[0]);
-        String numExperiments = args[1];
+        int numExperiments = Integer.parseInt(args[1]);
         String seed = args[2];
+        List<Float> volatileData;
+        List<Float> nonVolatileData;
+
 
 
         //TASK 1: Performance of programs with and without caching
 
         //loop 1: Volatile
+        long totalSum = 0;
+        long totalTime = 0;
+        volatileData = 
         //Experiment counter
-        for (int j = 0; j < 0; j++){
+        for (int j = 0; j < numExperiments; j++){
             //Start timer
             long runningTotal = 0;
             for(int i = 0; i < size; i++){
@@ -23,15 +31,18 @@ public class Main {
             }
             //End Timer
             //Add timer to data about experiments
-            //end experiment loop
+            totalTime += 0; //TODO ADD TIME
+            totalSum += runningTotal;
         }
-
+        //TODO it might be worth writing this information to a .csv file so I can format it
         int averageTimeVolatile; //= totalTime / experiments
-        int averageSumVolatile; // = totalSum / experiments
+        long averageSumVolatile = totalSum / (long) numExperiments;
 
         //loop 2: Non-Volatile
+        // will be the same just copy later
 
         //TASK 2
+        
 
         //TASK 3
 
