50e6ccde0fb0a39d6e10b0377507ae74912c2224
Tatiana Gabel <Tianamg78@gmail.com>
2023-09-29 16:26:37 -0600

    Some major shifts

--- hw2/Main.java
+++ hw2/Main.java
@@ -10,39 +10,10 @@ public class Main {
         String seed = args[2];
         List<Long> volatileData;
         List<Long> nonVolatileData;
+        List<Long> firstTenData;
+        List<Long> lastTenData;
 
         //TASK 1: Performance of programs with and without caching
-
-        //loop 1: Volatile
-        long totalSum = 0;
-        long totalTime = 0;
-        volatileData = new ArrayList<Long>();
-        //Experiment counter
-        for (int j = 0; j < numExperiments; j++){
-            //Start timer
-            long endTime;
-            long runningTotal = 0;
-            //v must be declared as part of the class
-            long startTime = System.nanoTime();
-            
-            while(v < size){
-                //running total of addition and subtraction operations using the loop variable
-                if(v % 2 == 0){
-                    runningTotal += v;
-                } else {
-                    runningTotal -= v;
-                }
-                v++;
-            }
-            //End Timer
-            endTime = System.nanoTime();
-            volatileData.add(new Long(endTime-startTime)); //Store individual data
-            totalTime += (endTime-startTime); // in Nano seconds
-            totalSum += runningTotal;
-        }
-        //TODO it might be worth writing this information to a .csv file so I can format it
-        long averageTimeVolatile = totalTime / numExperiments;
-        long averageSumVolatile = totalSum / numExperiments;
 
         //loop 2: Non-Volatile
         totalSum = 0;
@@ -81,13 +52,31 @@ public class Main {
         Integer[] ronaldo = new Integer[size];
         //populate the array with random numbers
 
-        //Experiments loop
-        //Start timer
-        //Access first 10%
-        //Stop timer
-        //start other timer
-        //Access random element in the last 10% of the array
-        //end other timer
+        Long firstTenTotalTime = (long)0;
+        Long lastTenTotalTime = (long)0;
+        firstTenData = new ArrayList<Long>();
+        lastTenData = new ArrayList<Long>();
+        //Experiment counter
+        for (int j = 0; j < numExperiments; j++){
+            //Part 1: Accessing the first 10%
+            //Start timer
+            long endTime;
+            long startTime = System.nanoTime();
+            //Access first 10%
+            //Stop timer
+            endTime = System.nanoTime();
+            firstTenData.add(new Long(endTime-startTime));
+            firstTenTotalTime += (endTime-startTime);
+
+            //Part 2: Accessing a random value in the last 10%
+            //start other timer
+            startTime = System.nanoTime();
+            //Access random element in the last 10% of the array
+            //end other timer
+            endTime = System.nanoTime();
+            lastTenData.add(new Long(endTime-startTime));
+            lastTenTotalTime += (endTime-startTime);
+        }
 
         //Average sum of the elements ???
 
@@ -107,5 +96,38 @@ public class Main {
         //Record data to totals + file
         //End experiments
 
+    }
+
+    public static ArrayList<Long> task1(ArrayList<Long> data, ){
+        //loop 1: Volatile
+        long totalSum = 0;
+        long totalTime = 0;
+        data = new ArrayList<Long>();
+        //Experiment counter
+        for (int j = 0; j < numExperiments; j++){
+            //Start timer
+            long endTime;
+            long runningTotal = 0;
+            //v must be declared as part of the class
+            long startTime = System.nanoTime();
+            
+            while(v < size){
+                //running total of addition and subtraction operations using the loop variable
+                if(v % 2 == 0){
+                    runningTotal += v;
+                } else {
+                    runningTotal -= v;
+                }
+                v++;
+            }
+            //End Timer
+            endTime = System.nanoTime();
+            data.add(new Long(endTime-startTime)); //Store individual data
+            totalTime += (endTime-startTime); // in Nano seconds
+            totalSum += runningTotal;
+        }
+        //TODO it might be worth writing this information to a .csv file so I can format it
+        long averageTimeVolatile = totalTime / numExperiments;
+        long averageSumVolatile = totalSum / numExperiments;
     }
 }
