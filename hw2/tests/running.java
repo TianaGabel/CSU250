5bf3d7f4c596eafb2cf9b590115c877a8d6548ee
Tatiana Gabel <Tianamg78@gmail.com>
2023-10-05 14:45:21 -0600

    test added

---
+++ hw2/tests/running.java
@@ -0,0 +1,8 @@
+package tests;
+
+public class running {
+
+    //Either we can set the debugger with command line arguments or prompt for them
+    //Look up a method of changing the input from a scanner to a text file?????
+    
+}
