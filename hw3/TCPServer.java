537102e7c0a3f736c7be588883978837ecf9b695
Tatiana Gabel <Tianamg78@gmail.com>
2023-11-25 16:02:32 -0700

    Added server to server communication

--- hw3/TCPServer.java
+++ hw3/TCPServer.java
@@ -7,6 +7,7 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class TCPServer {
@@ -15,6 +16,7 @@ public class TCPServer {
     private static Socket clientSocket1;
     private static Socket clientSocket2;
     private static DataOutputStream DataOutputToClient;
+    private static DataInputStream DataInputToClient;
     private static int seedNumber = 0;
     private static int numMessages = 0;
     private static int portNumber = Integer.MAX_VALUE;
@@ -53,13 +55,13 @@ public class TCPServer {
         
         try {
             serverSocket = new ServerSocket(portNumber);
-            System.out.println("Waiting for client");
+            System.out.println("waiting for client...");
 
             clientSocket1 = serverSocket.accept();
             clientSocket2 = serverSocket.accept();
-            System.out.println("Successfully connected to the clients");
+            System.out.println("Clients Connected!");
 
-            System.out.println("Sending config");
+            System.out.println("Sending config to clients...");
             sendConfigData(clientSocket1, randomNumber1);
             sendConfigData(clientSocket2, randomNumber2);
 
@@ -68,30 +70,26 @@ public class TCPServer {
             System.out.println("Finished sending config to clients.");
 
             System.out.println("Starting to listen for client messages...");
-            //TODO will this be stored before being relayed or be related on demand?
-
+            ArrayList<Integer> client1Messages = recieveMessages(clientSocket1);
+            ArrayList<Integer> client2Messages = recieveMessages(clientSocket2);
+            
 
             System.out.println("Finished Listening for client messages.");
             System.out.println(clientSocket1.getInetAddress().getHostName());
-            //TODO fill this in with the necessary information
-            System.out.println("Messages received: ");
-            System.out.println("Sum received: ");
+            System.out.println("\tMessages received: " + client1Messages.size());
+            int sum1 = 0;
+            for (int i: client1Messages) {sum1 += i;}
+            System.out.println("\tSum received: " + sum1);
+
             System.out.println(clientSocket2.getInetAddress().getHostName());
-            System.out.println("Messages received: ");
-            System.out.println("Sum received: ");
+            System.out.println("\tMessages received: " + client2Messages.size());
+            int sum2 = 0;
+            for (int i: client2Messages) {sum2 += i;}
+            System.out.println("\tSum received: " + sum2);
 
             //Relaying messages to clients
-            //relayClientToClientMessage();
-            //relayClientToClientMessage();
-            
-            
-
-            //future for submission 2
-            //Recieve numbers from client
-            DataInputStream inputData = new DataInputStream(clientSocket1.getInputStream());
-            for(int i = 0; i < numMessages; i++){
-
-            }
+            relayClientToClientMessage(clientSocket1,client2Messages);
+            relayClientToClientMessage(clientSocket2,client1Messages);
         } catch(IOException e){
             System.out.println("Could not listen on port " + portNumber);
             e.getMessage();
@@ -100,9 +98,26 @@ public class TCPServer {
         }
     }
 
-    public static void relayClientToClientMessage(Socket){
-       
+    private static ArrayList<Integer> recieveMessages(Socket clientSocket) throws IOException {
+        Integer receivedNum;
+        Integer receiverSum;
+        ArrayList<Integer> receivedNumbers = new ArrayList<>();
+        DataInputToClient = new DataInputStream(clientSocket.getInputStream());
+        for (int i = 0; i < numMessages; i++) {
+            receivedNum = DataInputToClient.readInt();
+            receivedNumbers.add(receivedNum);
+        }
+        return receivedNumbers;
     }
+
+    public static void relayClientToClientMessage(Socket clientSocket, ArrayList<Integer> messages) throws Exception{
+        DataOutputToClient = new DataOutputStream(clientSocket.getOutputStream());
+        for (int i: messages){
+            DataOutputToClient.writeInt(i);
+        }
+        DataOutputToClient.flush();
+    }
+    
     public static void sendConfigData(Socket clientSocket, int rand) throws Exception{
         //Send 2 numbers
             DataOutputToClient = new DataOutputStream(clientSocket.getOutputStream());
